3df7a9b99c80d80d8c6d47f8e8aa20a8b2c16ac7
hleb229 <151877181+hleb229@users.noreply.github.com>
2024-05-09 00:41:33 +0300

    Add files via upload

--- Open_eye_detector/Open_eye_detector.py
+++ Open_eye_detector/Open_eye_detector.py
@@ -2,7 +2,8 @@ import cv2
 import time
 from playsound import playsound
 
-last_time = 0.0  # Глобальная переменная для отслеживания времени последнего звука.
+last_time = 0  # Глобальная переменная для отслеживания времени последнего звука.
+
 
 def main():
     # Загрузка классификатора каскада Хаара для обнаружения глаз.
@@ -11,22 +12,21 @@ def main():
     def detect_eyes(img):
         global last_time  # Объявление глобальной переменной для доступа и изменения внутри функции.
         face_img = img.copy()  # Копирование изображения для обработки.
-
         # Обнаружение глаз на изображении.
-        eyes_rect = eye_cascade.detectMultiScale(face_img, scaleFactor=1.15, minNeighbors=25)
+        eyes_rect = eye_cascade.detectMultiScale(face_img, scaleFactor=1.15, minNeighbors=3)
         for (x, y, w, h) in eyes_rect:
-            if w >= 50:  # Проверка размера глаз. Если глаза слишком малы, они игнорируются.
-                # Отрисовка прямоугольника вокруг глаз.
-                cv2.rectangle(face_img, (x, y), (x + w, y + h), (0, 0, 255), 10)
-                if eyes_rect.all():  # Проверка, что глаза были обнаружены.
-                    # Добавление текста на изображение, указывающего на открытые глаза.
-                    cv2.putText(face_img, "EYES ARE OPEN", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                    # Проверка времени с момента последнего воспроизведения звука.
-                    if time.time() - last_time >= 1.2:
-                        # Воспроизведение звука.
-                        playsound("close-eyes.mp3", False)
-                        # Обновление времени последнего звука.
-                        last_time = time.time()
+            if w >= 10:  # Проверка размера глаз. Если глаза слишком малы, они игнорируются.
+                # Проверка местоположения глаз. Если глаза не попадают в область отверстий для глаз, они игнорируются.
+                if x >= 65 and y >= 245 and x <= 285 and y <= 415 or x >= 365 and y >= 245 and x <= 585 and y <= 415:
+                    # Отрисовка прямоугольника вокруг глаз.
+                    cv2.rectangle(face_img, (x, y), (x + w, y + h), (0, 0, 255), 5)
+                    if eyes_rect.any():  # Проверка, что глаза были обнаружены.
+                        # Добавление текста на изображение, указывающего на открытые глаза.
+                        cv2.putText(face_img, "ЗАКРОЙ ГЛАЗА!", (30, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                        # Проверка времени с момента последнего воспроизведения звука.
+                        if time.time() - last_time >= 1:
+                            playsound("close-eyes.mp3", False) # Воспроизведение звука.
+                            last_time = time.time() # Обновление времени последнего звука.
 
         return face_img  # Возвращение обработанного изображения.
 
@@ -43,5 +43,6 @@ def main():
 
     cap.release()  # Освобождение ресурсов камеры.
     cv2.destroyAllWindows()  # Закрытие всех окон OpenCV.
+
 
 main()  # Вызов функции main().
