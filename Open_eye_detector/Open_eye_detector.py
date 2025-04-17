994f391800ecaa3dfffe428393a6df8aee2e6690
hleb229 <151877181+hleb229@users.noreply.github.com>
2025-04-17 16:11:53 +0300

    Update Open_eye_detector.py

--- Open_eye_detector/Open_eye_detector.py
+++ Open_eye_detector/Open_eye_detector.py
@@ -1,6 +1,8 @@
 import cv2
 import time
-from playsound import playsound
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("close-eyes.mp3")
 
 last_time = 0  # Глобальная переменная для отслеживания времени последнего звука.
 
@@ -13,24 +15,24 @@ def main():
         global last_time  # Объявление глобальной переменной для доступа и изменения внутри функции.
         face_img = img.copy()  # Копирование изображения для обработки.
         # Обнаружение глаз на изображении.
-        eyes_rect = eye_cascade.detectMultiScale(face_img, scaleFactor=1.15, minNeighbors=3)
+        eyes_rect = eye_cascade.detectMultiScale(face_img, scaleFactor=1.15, minNeighbors=7)
         for (x, y, w, h) in eyes_rect:
-            if w >= 10:  # Проверка размера глаз. Если глаза слишком малы, они игнорируются.
+            if w >= 70:  # Проверка размера глаз. Если глаза слишком малы, они игнорируются.
                 # Проверка местоположения глаз. Если глаза не попадают в область отверстий для глаз, они игнорируются.
-                if x >= 65 and y >= 245 and x <= 285 and y <= 415 or x >= 365 and y >= 245 and x <= 585 and y <= 415:
+                if x >= 65 and y >= 400 and x <= 450 and y <= 675 or x >= 600 and y >= 430 and x <= 985 and y <= 705:
                     # Отрисовка прямоугольника вокруг глаз.
                     cv2.rectangle(face_img, (x, y), (x + w, y + h), (0, 0, 255), 5)
                     if eyes_rect.any():  # Проверка, что глаза были обнаружены.
                         # Добавление текста на изображение, указывающего на открытые глаза.
-                        cv2.putText(face_img, "ЗАКРОЙ ГЛАЗА!", (30, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                        cv2.putText(face_img, "ГЛАЗА ОТКРЫТЫ!", (30, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
                         # Проверка времени с момента последнего воспроизведения звука.
                         if time.time() - last_time >= 1:
-                            playsound("close-eyes.mp3", False) # Воспроизведение звука.
+                            pygame.mixer.music.play()
                             last_time = time.time() # Обновление времени последнего звука.
 
         return face_img  # Возвращение обработанного изображения.
 
-    cap = cv2.VideoCapture(2)  # Инициализация захвата видеопотока с камеры.
+    cap = cv2.VideoCapture(0)  # Инициализация захвата видеопотока с камеры.
 
     while True:
         ret, frame = cap.read(0)  # Захват кадра с камеры.
